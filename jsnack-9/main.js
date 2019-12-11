777740e15afd6afe8fca7c7ab3ec01727091c293
simonezanfagna <50368019+simonezanfagna@users.noreply.github.com>
2019-12-11 10:59:56 +0100

    Update main.js

--- jsnack-9/main.js
+++ jsnack-9/main.js
@@ -2,8 +2,8 @@ var lista = ['casa', 12, 3, 45, 'a', 10, 'b']
 
 function elementiCompresi(array,num_1,num_2) {
   array.splice(num_2,array.length);
-  array.splice(0,num_1);
+  array.splice(0,num_1 - 1);
   return array
 }
 console.log(lista);
-console.log(elementiCompresi(lista,1,4));
+console.log(elementiCompresi(lista,2,4));
